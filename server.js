486b560d9a6ce6591667d0b11bc3f71d21002833
alfirassmanar <alfirassmanar11@gmail.com>
2023-12-02 12:59:07 +0700

    BE-2-Jakarta-19-Railway
    for railway

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const server = express()
+
+// nambah router
+const allRouter = require("./routes")
+
+const PORT = process.env.PORT || 6978
+
+server.use(express.json())
+server.use(allRouter)
+
+server.listen(PORT, () =>{
+    console.log("server running on port", PORT);
+})
